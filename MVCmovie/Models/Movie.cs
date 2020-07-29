757b8a8cceb44f597dc31a2c78df0404a831ea45
heymiracleboy <61914827+heymiracleboy@users.noreply.github.com>
2020-07-29 18:37:36 +0930

    validation

--- MVCmovie/Models/Movie.cs
+++ MVCmovie/Models/Movie.cs
@@ -21,6 +21,10 @@ namespace MvcMovie.Models
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string Rating { get; set; }
     }
 }
