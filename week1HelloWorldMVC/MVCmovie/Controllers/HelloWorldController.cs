cfa82c132f21d5e81147c9528721ff34b6dbcba1
heymiracleboy <61914827+heymiracleboy@users.noreply.github.com>
2020-08-05 20:04:47 +0930

    some index routing changes

--- week1HelloWorldMVC/MVCmovie/Controllers/HelloWorldController.cs
+++ week1HelloWorldMVC/MVCmovie/Controllers/HelloWorldController.cs
@@ -5,12 +5,12 @@ namespace MvcMovie.Controllers
 {
         public class HelloWorldController : Controller
         {
-            public IActionResult Index()
-            {
-                return View();
-            }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-            public IActionResult Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
             {
                 ViewData["Message"] = "Hello " + name;
                 ViewData["NumTimes"] = numTimes;
